5422d61c6eb8b3ee565217d139cdb17b6383e3d4
jpickard393 <jpickard@and.digital>
2021-07-31 15:05:32 +0100

    Added mock to testisItemInDatabase function

--- __Tests__/quotes.test.js
+++ __Tests__/quotes.test.js
@@ -1,15 +1,15 @@
 import { mockWatchListItems } from "../__tests__/fixtures/watchlist_data";
-import { checkIfItemInWatchList, filterArray } from "../scripts/quotes";
+import { checkIfItemInWatchList, filterArray, isItemInDatabase } from "../scripts/quotes";
+
+let mockCheckIfItemInWatchList;
+
 
 describe('should return correct values from mockWatchListItems', () => {
-    let mockCheckIfItemInWatchList;
-
-    const initialiseMockData = () => {
-        mockCheckIfItemInWatchList = jest.fn(() => mockWatchListItems);
-    };
-
     // beforeAll runs before any of the tests are run so the data is set up at the start.
     beforeAll(() => {
+        const initialiseMockData = () => {
+            mockCheckIfItemInWatchList = jest.fn(() => mockWatchListItems);
+        };
         return initialiseMockData();
     });
 
@@ -25,30 +25,34 @@ describe('should return correct values from mockWatchListItems', () => {
 
     // This is testing that there is a key value pair of  "key": 2, "symbol": "MSFT" in the mock data
     it('should return return object from array containg specified key and value', () => {
-        expect(mockCheckIfItemInWatchList()).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({ "key": 2, "symbol": "MSFT" },)]
-            )
+        expect(mockWatchListItems).toEqual(
+            expect.arrayContaining([{ "key": 2, "symbol": "MSFT" }])
         );
     });
 
+    // test if we can do this with just the array
     // This is testing that there is not a key value pair of  "key": 99, "symbol": "AMZN" in the mock data
     it('should not return object from array containg an invalid key', () => {
         expect(mockCheckIfItemInWatchList()).toEqual(
-            expect.arrayContaining([
-                expect.not.objectContaining({ "key": 99, "symbol": "AMZN" },)]
+            expect.not.arrayContaining(
+                [{ "key": 99, "symbol": "AMZN" }]
             )
         );
     });
 });
 
-// These tests should test the actual function but with mock data, so we don;t have to rely on a database
+// These tests should test the actual function but with mock data, so we don't have to rely on a database
 // where the data could change and render the tests useless.
-describe('should return correct values from mockWatchListItems() by passing in mock data', () => {
-    it("should return true", () => {
-        const mockCheckIfItemInWatchList = jest.fn(() => checkIfItemInWatchList("GOOG", mockWatchListItems));
-        expect(mockCheckIfItemInWatchList()).toBe(true);
+describe('checkIfItemInWatchList', () => {
+    it("should identify when an item is in the array", () => {
+        const existingItem = "GOOG";
+        const result = checkIfItemInWatchList(existingItem, mockWatchListItems);
+
+        expect(result).toBe(true);
     });
+
+
+    // also do failing test
 
     it('should filter the array and return { "key": 1, "symbol": "AMZN" }', () => {
         const mockFilterArray = jest.fn(() => filterArray(symbol, mockWatchListItems));
@@ -60,5 +64,21 @@ describe('should return correct values from mockWatchListItems() by passing in m
                 expect.objectContaining(expected)]
             )
         );
+    });
+});
+
+describe('', () => {
+    it('should return { "key": 1, "symbol": "AMZN" }', () => {
+        // test isItemInDatabase by mocking second parameter (fetchAllItemsFromDatabaseFunction)
+        // fetchAllItemsFromDatabaseFunction is mocked to be mockWatchListItems, an array of objects
+
+        const fetchAllItemsFromDatabaseFunction = () => {
+            const result = jest.fn(() => mockWatchListItems);
+            return result();
+        };
+        const searchItem = "AMZN";
+        const expected = { "key": 1, "symbol": "AMZN" };
+        const mockFetchAllItemsFromDatabaseFunction = isItemInDatabase(searchItem, fetchAllItemsFromDatabaseFunction);
+        expect(mockFetchAllItemsFromDatabaseFunction).toEqual(expected);
     });
 });
--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,13 +1,3 @@
-// Pretend this data is comming from a database
-// const watchListItems = [
-//     { key: 1, symbol: "AMZN" },
-//     { key: 2, symbol: "MSFT" },
-//     { key: 3, symbol: "GOOG" },
-//     { key: 4, symbol: "AAPL" },
-//     { key: 5, symbol: "ARKK" }
-// ];
-
-
 export const checkIfItemInWatchList = (company, watchListItems) => {
     const check = watchListItems.find(
         (item) => item.symbol === company.toUpperCase()
@@ -18,3 +8,14 @@ export const checkIfItemInWatchList = (company, watchListItems) => {
 export const filterArray = (symbol, watchListItems) => {
     return watchListItems.filter((item) => item.symbol.includes(symbol));
 };
+
+const fetchAllItemsFromDatabase = () => {
+    // this would return a list of items from the database,
+    // but for this example we will not bother connecting to a database
+
+};
+
+export const isItemInDatabase = (symbol, fetchAllItemsFromDatabaseFunction) => {
+    const data = fetchAllItemsFromDatabaseFunction();
+    return data.find((searchItem) => searchItem.symbol === symbol);
+}
