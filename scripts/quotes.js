eac2c57750f2e40aa13dd195239fd5e5b7df8ffa
pickard, julian <julian.pickard@sky.uk>
2021-09-01 10:20:17 +0100

    latest changes after jose

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -1,22 +1,22 @@
 export const checkIfItemInWatchList = (company, watchListItems) => {
-    const check = watchListItems.find(
-        (item) => item.symbol === company.toUpperCase()
-    );
-    return typeof check !== "undefined";
+  const check = watchListItems.find(
+    (item) => item.symbol === company.toUpperCase()
+  );
+  return typeof check !== "undefined";
 };
 
 export const filterArray = (symbol, watchListItems) => {
-    return watchListItems.filter((item) => item.symbol.includes(symbol));
+  return watchListItems.filter((item) => item.symbol.includes(symbol));
 };
 
 const fetchAllItemsFromDatabase = () => {
-    // this would return a list of items from the database,
-    // but for this example we will not bother connecting to a database
-    // this will be mocked from the tests
-
+  // this would return a list of items from the database,
+  // but for this example we will not bother connecting to a database
+  // this will be mocked from the tests
 };
 
 export const isItemInDatabase = (symbol, fetchAllItemsFromDatabaseFunction) => {
-    const data = fetchAllItemsFromDatabaseFunction();
-    return data.find((searchItem) => searchItem.symbol === symbol);
-}
+  // calls function I just passed in (which was mocked)
+  const data = fetchAllItemsFromDatabaseFunction();
+  return data.find((searchItem) => searchItem.symbol === symbol);
+};
